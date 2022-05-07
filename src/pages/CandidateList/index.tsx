bc5fce8cdf601773ca319dbd92ea9c9d983e6063
Klinton <chhunklinton91@gmail.com>
2022-05-07 11:08:48 +0700

    setup chakra

---
+++ src/pages/CandidateList/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from '@chakra-ui/react';
+
+const CandidateList = () => <Text color="primary">Candidate</Text>;
+
+export default CandidateList;
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  primary: '#07B53B',
+  textColor: '#1E1E1E',
+  textColorGray: '#777777',
+};
+
+const fonts = {
+  heading: 'Noto Sans JP, sans-serif',
+  body: 'Noto Sans JP, sans-serif',
+};
+
+export default extendTheme({ colors, fonts });
